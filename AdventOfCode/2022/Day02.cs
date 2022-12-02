692d3d76c8b1a6facb1c7ac9c085952defd35bdc
Martin Rölin <martin.rolin@vitecsoftware.com>
2022-12-02 06:27:57 +0100

    Dag 2

--- AdventOfCode/2022/Day02.cs
+++ AdventOfCode/2022/Day02.cs
@@ -12,9 +12,73 @@ namespace AdventOfCode._2022
         {
             long part1 = 0;
             long part2 = 0;
-            string allText = File.ReadAllText("Input\\2022\\day01.txt");
+            string allText = File.ReadAllText("Input\\2022\\day02.txt");
+            var listOfValues = allText.Split("\r\n").ToList();
 
-            WriteResult(2, part1, part2, result.none);
+            for (int i = 0; i < listOfValues.Count; i++)
+            {
+                var start = listOfValues[i].Split(" ")[0];
+                var response = listOfValues[i].Split(" ")[1];
+                
+
+                if (start == "A") {                    
+                    if (response == "X")
+                    {
+                        part1 += 1 + 3;
+                        part2 += 3 + 0;
+                    }
+                    if (response == "Y")
+                    {
+                        part1 += 2 + 6;
+                        part2 += 1 + 3;
+
+                    }
+                    if (response == "Z")
+                    {
+                        part1 += 3 + 0;
+                        part2 += 2 + 6;
+                    }
+                }
+                if (start == "B")
+                {
+                    
+                    if (response == "X")
+                    {
+                        part1 += 1 + 0;
+                        part2 += 1 + 0;
+                    }
+                    if (response == "Y")
+                    {
+                        part1 += 2 + 3;
+                        part2 += 2 + 3;
+                    }
+                    if (response == "Z")
+                    {
+                        part1 += 3 + 6;
+                        part2 += 3 + 6;
+                    }
+                }
+                if (start == "C")
+                {                    
+                    if (response == "X")
+                    {
+                        part1 += 1 + 6;
+                        part2 += 2 + 0;
+                    }
+                    if (response == "Y")
+                    {
+                        part1 += 2 + 0;
+                        part2 += 3 + 3;
+                    }
+                    if (response == "Z")
+                    {
+                        part1 += 3 + 3;
+                        part2 += 1 + 6;
+                    }
+                }
+            }
+
+            WriteResult(2, part1, part2, result.gold);
 
         }
     }
---
+++ AdventOfCode/2022/Day03.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode._2022
+{
+    class Day03 : Helper
+    {
+        public void Solve()
+        {
+            long part1 = 0;
+            long part2 = 0;
+            string allText = File.ReadAllText("Input\\2022\\day02.txt");
+            var listOfValues = allText.Split("\r\n").ToList();
+
+
+
+            WriteResult(3, part1, part2, result.none);
+
+        }
+    }
+}
+
+
+
+
